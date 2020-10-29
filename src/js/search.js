fc93cf9571387b7a584c3c67b74a282c011d0b16
olena-kharchenko <harchenko.es1990@gmail.com>
2020-10-29 12:22:16 +0200

    feat: add markup and notification

--- src/js/search.js
+++ src/js/search.js
@@ -1,9 +1,15 @@
 var debounce = require('lodash.debounce');
 import API from './fetchCountries.js';
+import listOfContriesTpl from '../templates/list-of-countries.hbs';
+import countryCardTpl from '../templates/country-markup.hbs';
+
+import { info, error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
 
 const inputEl = document.querySelector('.input-search');
 const cardContainer = document.querySelector('.js-card-container');
-let countryToSearch = 'ukr';
+let countryToSearch = '';
 
 inputEl.addEventListener(
   'input',
@@ -17,16 +23,31 @@ function onSearch() {
   console.log(countryToSearch);
 
   API.fetchCountries(countryToSearch)
-    .then(renderCountryCard)
+    .then(checkingNumberOfCountries)
     .catch(onFetchError);
 }
 
-function renderCountryCard(country) {
-  console.log(country);
-  //   const markup = countryCardTpl(country);
-  //   cardContainer.innerHTML = markup;
+function checkingNumberOfCountries(countries) {
+  if (countries.length > 10) {
+    error({
+      title: 'Too many matches found.',
+      text: 'Please enter a more specific query!',
+      delay: 2000,
+      closerHover: true,
+    });
+  } else if (countries.length <= 10 && countries.length > 1) {
+    renderMarkup(listOfContriesTpl, countries);
+  } else {
+    renderMarkup(countryCardTpl, countries[0]);
+  }
+}
+
+function renderMarkup(template, countries) {
+  const markup = template(countries);
+  cardContainer.innerHTML = markup;
 }
 
 function onFetchError(error) {
+  cardContainer.innerHTML = '';
   console.log(error);
 }
